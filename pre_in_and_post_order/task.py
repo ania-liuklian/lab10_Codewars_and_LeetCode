d957f4e54cfeab9adc30c00c75235ece78195bbc
Liuklian Anna <liuklian.pn@ucu.edu.ua>
2026-04-20 20:22:41 +0300

    Add Binary Tree Traversal solution

---
+++ pre_in_and_post_order/task.py
@@ -0,0 +1,21 @@
+# Pre-order traversal
+"""Module"""
+def pre_order(node):
+    """pre"""
+    if not node:
+        return []
+    return [node.data] + pre_order(node.left) + pre_order(node.right)
+
+# In-order traversal
+def in_order(node):
+    """in"""
+    if not node:
+        return []
+    return in_order(node.left) + [node.data] + in_order(node.right)
+
+# Post-order traversal
+def post_order(node):
+    """post"""
+    if not node:
+        return []
+    return post_order(node.left) + post_order(node.right) + [node.data]
